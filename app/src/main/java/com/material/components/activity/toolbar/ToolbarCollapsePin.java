df908068f0ccca96646bdbd91977e401df6c1b30
Imran <imranhamzah@gmail.com>
2017-12-31 18:03:44 +0800

    Update popupmenu on lesson page.

--- app/src/main/java/com/material/components/activity/toolbar/ToolbarCollapsePin.java
+++ app/src/main/java/com/material/components/activity/toolbar/ToolbarCollapsePin.java
@@ -1,25 +1,39 @@
 package com.material.components.activity.toolbar;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.material.components.R;
 
 public class ToolbarCollapsePin extends AppCompatActivity {
 
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar_collapse_pin);
+        context = getApplicationContext();
         initToolbar();
+    }
+
+    public void openSubMenu(View v)
+    {
+        PopupMenu popup = new PopupMenu(v.getContext(), v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_product_more, popup.getMenu());
+        popup.show();
     }
 
     private void initToolbar() {
