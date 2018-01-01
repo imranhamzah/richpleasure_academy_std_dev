c78b7e08b49e0bf256ffeaec06873beea5e5e9c8
Imran <imranhamzah@gmail.com>
2018-01-01 09:56:37 +0800

    Update dashboard and logout links.

--- app/src/main/java/com/material/components/activity/menu/MenuDrawerNews.java
+++ app/src/main/java/com/material/components/activity/menu/MenuDrawerNews.java
@@ -8,7 +8,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -17,15 +19,20 @@ import android.view.View;
 
 import com.material.components.R;
 import com.material.components.activity.MainMenu;
+import com.material.components.activity.login.LoginActivity;
+import com.material.components.activity.login.SQLiteHandler;
+import com.material.components.activity.login.SessionManager;
 import com.material.components.activity.profile.ProfilePolygon;
 import com.material.components.activity.search.SearchToolbarLight;
 import com.material.components.activity.toolbar.ToolbarCollapsePin;
 import com.material.components.adapter.AdapterGridShopProductCard;
 import com.material.components.data.DataGenerator;
+import com.material.components.model.ShopCategory;
 import com.material.components.model.ShopProduct;
 import com.material.components.utils.Tools;
 import com.material.components.widget.SpacingItemDecoration;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MenuDrawerNews extends AppCompatActivity {
@@ -37,6 +44,9 @@ public class MenuDrawerNews extends AppCompatActivity {
     private AdapterGridShopProductCard mAdapter;
     private View parent_view;
 
+    private SessionManager session;
+    private SQLiteHandler db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +56,22 @@ public class MenuDrawerNews extends AppCompatActivity {
         initToolbar();
         initNavigationMenu();
         initComponent();
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
     }
 
     private void initToolbar() {
@@ -101,24 +127,47 @@ public class MenuDrawerNews extends AppCompatActivity {
                     Intent intentProfile = new Intent(getApplicationContext(), MainMenu.class);
                     startActivity(intentProfile);
                 }
+                if(id == R.id.logout)
+                {
+                    logoutUser();
+                }
                 drawer.closeDrawers();
                 return true;
             }
         });
     }
 
+
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MenuDrawerNews.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
     private void initComponent() {
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-        recyclerView.addItemDecoration(new SpacingItemDecoration(2, Tools.dpToPx(this, 8), true));
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setNestedScrollingEnabled(false);
 
         List<ShopProduct> items = DataGenerator.getShoppingProduct(this);
 
         //set data and list adapter
         mAdapter = new AdapterGridShopProductCard(this, items);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setNestedScrollingEnabled(false);
         recyclerView.setAdapter(mAdapter);
+
+        RecyclerView.LayoutManager layoutManagerSubject = new LinearLayoutManager(getApplicationContext());
+        recyclerView.addItemDecoration(new SpacingItemDecoration(2, Tools.dpToPx(this, 8), true));
+        recyclerView.setLayoutManager(layoutManagerSubject);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+        LinearLayoutManager linearLayoutManagerSubject = (LinearLayoutManager) layoutManagerSubject;
+        linearLayoutManagerSubject.setOrientation(LinearLayoutManager.HORIZONTAL);
 
         // on item list clicked
         mAdapter.setOnItemClickListener(new AdapterGridShopProductCard.OnItemClickListener() {
